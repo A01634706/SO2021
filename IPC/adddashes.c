753b4a06e9d39308f4b3459a53fb3812927ddb4d
A01634706 <A01634706@itesm.mx>
2021-04-21 00:07:08 +0000

    practica 6

---
+++ IPC/adddashes.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+
+
+
+int main(){
+    int fd[2]; 
+    int pid;
+    pipe(fd);
+    pid = fork();
+    if(pid == 0){
+        close(fd[1]);
+        dup2(fd[0],STDIN_FILENO);
+        execl("/usr/bin/tr","tr","' ","-",NULL);
+    }
+    if(pid > 0){//Proceso padre
+        close(fd[0]);
+        dup2(fd[1],STDOUT_FILENO);
+        execl("/usr/bin/cat","cat",NULL);
+    }
+	return 0;
+}
